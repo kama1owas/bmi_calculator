e14fb0e15ce32076b0dde7f51cc036b9a6eeaa61
kama1owas <kamalovas.00@gmail.com>
2023-04-18 16:07:52 +0600

    added black color

--- lib/constants/colors/app_colors.dart
+++ lib/constants/colors/app_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color mainColor = Color(0xff0A001F);
   static const Color whiteColor = Colors.white;
+  static const Color black = Color(0xff000000);
   static const Color greenColor = Color(0xff08AF28);
   static const Color activeColor = Colors.white;
   static const Color inactiveColor = Color(0xff0A001F);
